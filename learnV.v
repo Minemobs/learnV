048322cbbf56d57a4cd69e6b1f15ba9955006f0a
minemobs <issahakim94@gmail.com>
2021-07-23 20:28:56 +0200

    Added some string test functions

--- learnV.v
+++ learnV.v
@@ -3,6 +3,7 @@ module main
 fn main() {
 	h, w := hw()
 	println('$h $w')
+	test_string()
 	println(add(10, 5))
 	println(sub(10, 5))
 	println(mul(5, 2))
@@ -27,4 +28,11 @@ fn div(x int, y int) int {
 
 fn hw() (string, string) {
 	return "Hello", "World"
+}
+
+fn test_string() {
+	name := 'Minemobs'
+	println("Name Length : $name.len")
+	println("First letter : ${name[0]}")
+	println("Slicing : ${name[1..3]}")
 }
